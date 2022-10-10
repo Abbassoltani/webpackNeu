f55d8855ab7d310b9157c1e56b1ea362595d493a
abbas <abbassoltani35@gmail.com>
2022-10-11 01:01:47 +0200

    new

---
+++ src/add-imgae.js
@@ -0,0 +1,13 @@
+// import kivi from './wonder.jpg'
+
+// function addImage() {
+//     const img = document.createElement('img');
+//     img.alt = 'Kiwi';
+//     img.width = '458';
+//     img.src = kivi
+
+//     const body = document.querySelector('body');
+//     body.appendChild(img);
+// }
+
+// export default addImage;
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import HelloWorldButton from "./components/hello-world-buttom/hello-world-button.js";
+// import addImage from "./add-imgae.js";
+
+let helloWorld = new HelloWorldButton();
+helloWorld.render();
+// addImage();
